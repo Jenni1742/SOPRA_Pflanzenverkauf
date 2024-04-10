054de5ba2f7e2b41325c85486bc4a52169369abe
st184133 <st184133@stud.uni-stuttgart.de>
2024-04-10 16:59:44 +0200

    Initial commit Aufsetzen des Projekts

---
+++ src/main/java/com/example/sopra_pflanzenverkauf/SopraPflanzenverkaufApplication.java
@@ -0,0 +1,13 @@
+package com.example.sopra_pflanzenverkauf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopraPflanzenverkaufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopraPflanzenverkaufApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sopra_pflanzenverkauf/SopraPflanzenverkaufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sopra_pflanzenverkauf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopraPflanzenverkaufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
