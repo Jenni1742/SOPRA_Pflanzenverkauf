75bd6c39cfd830548243f52cbef0e104a3f6c5f0
st184133 <st184133@stud.uni-stuttgart.de>
2024-04-10 18:11:51 +0200

    User Klasse implementiert

---
+++ src/main/java/com/example/sopra_pflanzenverkauf/entity/User.java
@@ -0,0 +1,46 @@
+package com.example.sopra_pflanzenverkauf.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer userId;
+
+    private String username;
+
+    private String password;
+
+    public User() {
+        // empty constructor for Hibernate
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
