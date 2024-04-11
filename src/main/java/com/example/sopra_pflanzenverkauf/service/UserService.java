aa0a9c0606a17743261e0915953b918d118525fa
st184133 <st184133@stud.uni-stuttgart.de>
2024-04-11 20:10:54 +0200

    UserService ergänzen

--- src/main/java/com/example/sopra_pflanzenverkauf/service/UserService.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/service/UserService.java
@@ -7,7 +7,7 @@ import com.example.sopra_pflanzenverkauf.repository.BenutzerRepository;
 
 import java.util.List;
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
     @Autowired
 
     private BenutzerRepository userRepository;
@@ -20,4 +20,64 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    /**
+     * Sucht nach einem User mit einem bestimmten Usernamen.
+     *
+     * @param username der username.
+     * @return User-Objekt.
+     */
+    public Benutzer getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Spring Security Authentication Methoden
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Gibt den aktuell eingeloggten User zurück.
+     *
+     * @return User.
+     */
+    public Benutzer getCurrentUser() {
+        return getBenutzerByUsername(((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal()).getUsername());
+    }
+
+    /**
+     * Gibt das UserDetails Objekt des aktuell eingeloggten Users zurück. Wird u.U. benötigt um
+     * Rollen-Authentifizierungschecks durchzuführen.
+     *
+     * @return UserDetails Objekt der Spring Security.
+     */
+    public org.springframework.security.core.userdetails.User getCurrentUserDetails() {
+        return (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+    }
+
+    /**
+     * Überschreibt die Methode, welche für den Login der Spring Security benötigt wird..
+     *
+     * @param username the username des Benutzers.
+     * @return UserDetails Objekt des Spring Security Frameworks.
+     * @throws UsernameNotFoundException exception.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Benutzer user = userRepository.findByUsername(username);
+        if (Objects.isNull(user)) {
+            throw new UsernameNotFoundException("Could not find the user for username " + username);
+        }
+        List<GrantedAuthority> grantedAuthorities = getUserAuthorities(user.getRoles());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isEnabled(), true, true, user.isEnabled(), grantedAuthorities);
+    }
+
+    private List<GrantedAuthority> getUserAuthorities(Set<Rolle> roleSet) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Rolle role : roleSet) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getRolename()));
+        }
+        return grantedAuthorities;
+    }
 }
