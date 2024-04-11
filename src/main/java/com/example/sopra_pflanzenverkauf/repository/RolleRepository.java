2e152be2928fb012e33d7cf0de13dd4b76ef6ebf
st184133 <st184133@stud.uni-stuttgart.de>
2024-04-11 19:54:31 +0200

    RolleRepository implementieren

---
+++ src/main/java/com/example/sopra_pflanzenverkauf/repository/RolleRepository.java
@@ -0,0 +1,7 @@
+package com.example.sopra_pflanzenverkauf.repository;
+
+import com.example.sopra_pflanzenverkauf.entity.Rolle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolleRepository extends JpaRepository<Rolle, Integer>{
+}
