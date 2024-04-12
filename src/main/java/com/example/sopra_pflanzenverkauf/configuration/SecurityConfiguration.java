87e01ddad66bb953eb0fd1b5b677a30bfa9b6b6f
st184133 <st184133@stud.uni-stuttgart.de>
2024-04-12 17:11:13 +0200

    SecurityConfiguration überarbeitet

--- src/main/java/com/example/sopra_pflanzenverkauf/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/configuration/SecurityConfiguration.java
@@ -1,5 +1,5 @@
 package com.example.sopra_pflanzenverkauf.configuration;
-/*
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,28 +16,27 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.
-                authorizeHttpRequests()
-                // alle Requests die ohne Login erreichbar sind
-                .requestMatchers("/login", "/register", "/console/**").permitAll()
-                // definiere alle URLs die nur für eine bestimmte Rolle zugänglich sind
-                // Achtung: Spring Security fügt automatisch das Prefix "ROLE_" für die Überprüfung ein. Daher verwenden wir
-                // hier nicht "ROLE_ADMIN", wie bspw. im TestDataLoader angegeben.
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                // alle weiteren Requests erfordern Authentifizierung
-                .anyRequest().authenticated()
-                // füge CSRF token ein, welches evtl. für AJAX-requests benötigt wird
-                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringAntMatchers("/console/**")
-                // Request zum Aufruf der Login-Seite
-                .and().formLogin().loginPage("/login").failureUrl("/login?error=true").permitAll()
-                .defaultSuccessUrl("/", true)
-                .usernameParameter("username")
-                .passwordParameter("password")
-                // jeder kann sich ausloggen über den simplen /logout request ausloggen
-                .and().logout().permitAll()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout");
+        http.authorizeHttpRequests()
+            // alle Requests die ohne Login erreichbar sind
+            .requestMatchers("/login", "/register", "/console/**").permitAll()
+            // definiere alle URLs die nur für eine bestimmte Rolle zugänglich sind
+            // Achtung: Spring Security fügt automatisch das Prefix "ROLE_" für die Überprüfung ein. Daher verwenden wir
+            // hier nicht "ROLE_ADMIN", wie bspw. im TestDataLoader angegeben.
+                //.requestMatchers("/admin/**").hasRole("ADMIN")
+            // alle weiteren Requests erfordern Authentifizierung
+            .anyRequest().authenticated()
+            // füge CSRF token ein, welches evtl. für AJAX-requests benötigt wird
+            .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .ignoringAntMatchers("/console/**")
+            // Request zum Aufruf der Login-Seite
+            .and().formLogin().loginPage("/login").failureUrl("/login?error=true").permitAll()
+            .defaultSuccessUrl("/", true)
+            .usernameParameter("username")
+            .passwordParameter("password")
+            // jeder kann sich ausloggen über den simplen /logout request ausloggen
+            .and().logout().permitAll()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/login?logout");
 
         // Deaktiviert header security. Ermöglicht Nutzung der H2 Console.
         http.headers().frameOptions().sameOrigin().disable();
@@ -58,10 +57,9 @@ public class SecurityConfiguration {
      *
      * @return BCryptPasswordEncoder bean.
      */
-/*
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-*/
