255870f2413e75394149688e010819381e85e406
st184133 <st184133@stud.uni-stuttgart.de>
2024-04-12 16:50:05 +0200

    Testbenutzer erstellt ohne Rolle

--- src/main/java/com/example/sopra_pflanzenverkauf/service/TestDataLoader.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/service/TestDataLoader.java
@@ -1,11 +1,11 @@
 package com.example.sopra_pflanzenverkauf.service;
-/*
+
 import com.example.sopra_pflanzenverkauf.entity.Benutzer;
-import com.example.sopra_pflanzenverkauf.entity.Rolle;
+//import com.example.sopra_pflanzenverkauf.entity.Rolle;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-import com.example.sopra_pflanzenverkauf.service.RolleService;
+//import com.example.sopra_pflanzenverkauf.service.RolleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +14,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.HashSet;
 import java.util.Set;
 
-/*
+
 @Component
 public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent> {
     private static final Logger logger = LoggerFactory.getLogger(TestDataLoader.class);
 
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    //@Autowired
+    //private BCryptPasswordEncoder passwordEncoder;
 
-    @Autowired
-    private RolleService rolleService;
+    //@Autowired
+    //private RolleService rolleService;
 
     @Autowired
     private UserService userService;
@@ -33,13 +33,13 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
      * ausgeführt, wenn der Spring Kontext initialisiert wurde, d.h. wenn Sie Ihren Server (neu-)starten.
      */
 
-/*
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         logger.info("Initialisiere Datenbank mit Testdaten...");
 
         // Initialisieren Sie Beispielobjekte und speichern Sie diese über Ihre Services
-        Rolle userRole = new Rolle("ROLE_USER");
+        /*Rolle userRole = new Rolle("ROLE_USER");
         Rolle adminRole = new Rolle("ROLE_ADMIN");
         rolleService.saveRolle(userRole);
         rolleService.saveRolle(adminRole);
@@ -48,19 +48,26 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         userRoles.add(userRole);
 
         Set<Rolle> adminRoles = new HashSet<>();
-        adminRoles.add(adminRole);
+        adminRoles.add(adminRole);*/
 
-        Benutzer normalUser = new Benutzer();
-        normalUser.setUsername("user");
-        normalUser.setPassword(passwordEncoder.encode("1234"));
-        normalUser.setRoles(userRoles);
-        userService.saveUser(normalUser);
+        Benutzer jenniferKaisner = new Benutzer();
+        jenniferKaisner.setUsername("JenniferK");
+        jenniferKaisner.setPassword("1234");
+        jenniferKaisner.setFirstName("Jennifer");
+        jenniferKaisner.setLastName("Kaisner");
+        jenniferKaisner.setEmail("Jennifer.Kaisner@beispiel.de");
+        jenniferKaisner.setPlz("72108");
+        //jenniferKaisner.setPassword(passwordEncoder.encode("1234"));
+        //jenniferKaisner.setRoles(userRoles);
+        userService.saveUser(jenniferKaisner);
 
+        /*
         Benutzer admin = new Benutzer();
         admin.setUsername("admin");
-        admin.setPassword(passwordEncoder.encode("admin"));
-        admin.setRoles(adminRoles);
+        admin.setPassword("1234");
+        //admin.setPassword(passwordEncoder.encode("admin"));
+        //admin.setRoles(adminRoles);
         userService.saveUser(admin);
+        */
     }
 }
-*/
