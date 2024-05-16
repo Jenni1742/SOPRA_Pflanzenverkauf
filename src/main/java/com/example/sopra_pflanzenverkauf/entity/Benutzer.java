a6a589b369ac694e12b54df0e62f51f9a561a8b4
st183124 <st183124@stud.uni-stuttgart.de>
2024-05-16 16:35:32 +0200

    mehr Methoden in Benutzer und Rolle kommentiert

--- src/main/java/com/example/sopra_pflanzenverkauf/entity/Benutzer.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/entity/Benutzer.java
@@ -12,6 +12,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * The type Benutzer.
+ */
 @Entity
 public class Benutzer {
 
@@ -40,78 +43,171 @@ public class Benutzer {
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Rolle> roles;
 
+    /**
+     * Instantiates a new Benutzer.
+     */
     public Benutzer() {
         // empty constructor for Hibernate
     }
 
+    /**
+     * Gets user id.
+     *
+     * @return the user id
+     */
     public Integer getUserId() {
         return userId;
     }
 
+    /**
+     * Sets user id.
+     *
+     * @param userId the user id
+     */
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
 
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets password.
+     *
+     * @return the password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets password.
+     *
+     * @param password the password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Gets email.
+     *
+     * @return the email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets email.
+     *
+     * @param email the email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets plz.
+     *
+     * @return the plz
+     */
     public String getPlz() {
         return plz;
     }
 
+    /**
+     * Sets plz.
+     *
+     * @param plz the plz
+     */
     public void setPlz(String plz) {
         this.plz = plz;
     }
 
+    /**
+     * Is enabled boolean.
+     *
+     * @return the boolean
+     */
     public boolean isEnabled() {
         return enabled;
     }
 
+    /**
+     * Sets enabled.
+     *
+     * @param enabled the enabled
+     */
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
 
+    /**
+     * Gets roles.
+     *
+     * @return the roles
+     */
     public Set<Rolle> getRoles() {
         return roles;
     }
 
+    /**
+     * Sets roles.
+     *
+     * @param roles the roles
+     */
     public void setRoles(Set<Rolle> roles) {
         this.roles = roles;
     }
--- src/main/java/com/example/sopra_pflanzenverkauf/entity/Rolle.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/entity/Rolle.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * The type Rolle.
+ */
 @Entity
 public class Rolle implements GrantedAuthority {
     @Id
@@ -13,10 +16,18 @@ public class Rolle implements GrantedAuthority {
 
     private String rolename;
 
+    /**
+     * Instantiates a new Rolle.
+     */
     public Rolle() {
         //empty constructor for Hibernate
     }
 
+    /**
+     * Instantiates a new Rolle.
+     *
+     * @param rolename the rolename
+     */
     public Rolle(String rolename) {
         this.rolename = rolename;
     }
@@ -30,19 +41,38 @@ public class Rolle implements GrantedAuthority {
     ////////////////////////Aus Demo Projekt
 
 
-
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Gets rolename.
+     *
+     * @return the rolename
+     */
     public String getRolename() {
         return rolename;
     }
 
+    /**
+     * Sets rolename.
+     *
+     * @param rolename the rolename
+     */
     public void setRolename(String rolename) {
         this.rolename = rolename;
     }
