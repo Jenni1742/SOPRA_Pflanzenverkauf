6a8929694a57038b2fbe01eb8f40326c707391d2
Jenni1742 <166017145+Jenni1742@users.noreply.github.com>
2024-05-27 13:51:31 +0200

    Erklärende Kommentierung

--- src/main/java/com/example/sopra_pflanzenverkauf/repository/BenutzerRepository.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/repository/BenutzerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BenutzerRepository extends JpaRepository<Benutzer, Integer> {
 
-    Benutzer findByUsername(String username);
+    Benutzer findByUsername(String username); //"findBy" ist Schlüsselwort für SELECT * FROM Benutzer WHERE username = x
 
 }
