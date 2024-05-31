2f93578153366637a5675511b6835c7ff608f448
st182373 <st182373@stud.uni-stuttgart.de>
2024-05-31 14:25:33 +0200

    Model zu Controller hinzugefügt

--- src/main/java/com/example/sopra_pflanzenverkauf/controller/MyAdvertisementsController.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/MyAdvertisementsController.java
@@ -3,6 +3,7 @@ package com.example.sopra_pflanzenverkauf.controller;
 import com.example.sopra_pflanzenverkauf.service.PlantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,7 +21,8 @@ public class MyAdvertisementsController {
      * @return  the advertisements page
      */
     @RequestMapping(value = "/myAdvertisements", method = RequestMethod.GET)
-    public String showMyAdvertisementsPage(Map<String, Object> model) {
+    public String showMyAdvertisementsPage(Model model) {
+
         return "myAdvertisements";
     }
 }
