9ca15ce11ef96059252ea77d4f99c35df7cf6102
st182373 <st182373@stud.uni-stuttgart.de>
2024-06-05 11:34:27 +0200

    Ganze Verkaufsanzeige mit Beschreibung etc in neuem Fenster anzeigen lassen - Problem: komme bisher nicht an die aktuelle Pflanze

---
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/ShowAdvertisementController.java
@@ -0,0 +1,28 @@
+package com.example.sopra_pflanzenverkauf.controller;
+
+import com.example.sopra_pflanzenverkauf.entity.Plant;
+import com.example.sopra_pflanzenverkauf.service.PlantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ShowAdvertisementController {
+    @Autowired
+    private PlantService plantService;
+
+    /**
+     * Handles GET requests targeted at the showAdvertisement page.
+     *
+     * @return  the showAdvertisement page
+     */
+    @RequestMapping(value = "/showAdvertisement", method = RequestMethod.GET)
+    public String showAdvertisementPage(Model model) {
+
+        Plant currentPlant = plantService.getPlantByPlantId(1);
+
+        return "showAdvertisement";
+    }
+}
