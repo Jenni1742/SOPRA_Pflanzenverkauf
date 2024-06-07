6ee6baf0375f49bab5a6eac3828d58b32bb87705
Jenni1742 <166017145+Jenni1742@users.noreply.github.com>
2024-06-07 18:50:06 +0200

    Auf der Startseite geht nun das hinzufügen und entfernen von der Merkliste

--- src/main/java/com/example/sopra_pflanzenverkauf/controller/HomeController.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/HomeController.java
@@ -2,15 +2,18 @@ package com.example.sopra_pflanzenverkauf.controller;
 
 import com.example.sopra_pflanzenverkauf.entity.Category;
 import com.example.sopra_pflanzenverkauf.entity.Plant;
+import com.example.sopra_pflanzenverkauf.entity.User;
 import com.example.sopra_pflanzenverkauf.service.PlantService;
 import com.example.sopra_pflanzenverkauf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Controller
@@ -22,9 +25,12 @@ public class HomeController {
     private PlantService plantService;
 
     @GetMapping("/")
-    public String showHome(Model model, @RequestParam(value = "query", required = false) String query,
+    public String showHome(Model model,
+                           @RequestParam(value = "query", required = false) String query,
                            @RequestParam(value = "category", required = false) Category category,
                            @RequestParam(value = "price", required = false) String price)  {
+
+        User currentUser = userService.getCurrentUser();
         model.addAttribute("currentUser", userService.getCurrentUser());
 
         if (query != null && !query.isEmpty()) {
@@ -46,5 +52,32 @@ public class HomeController {
                 .collect(Collectors.toList());
         model.addAttribute("plants", plants);
         return "home"; // Name der HTML-Datei, die die Pflanzenliste anzeigt
+    }
+
+    @PostMapping(path = "/")
+    public String addToWishlist(@RequestParam("plant") Integer plant,
+                              Map<String, Object> model) {
+        User currentUser = userService.getCurrentUser();
+
+        currentUser.getWishlistPlants().add(plantService.getPlantByPlantId(plant));
+
+        userService.updateWishlist(currentUser);
+
+        model.put("currentUser", currentUser);
+        return "redirect:/";
+    }
+
+    @PostMapping(path = "/delete")
+    public String removePlantFromWishlist(@RequestParam("plant") Integer plant,
+                              Map<String, Object> model) {
+        User currentUser = userService.getCurrentUser();
+
+        currentUser.getWishlistPlants().remove(plantService.getPlantByPlantId(plant));
+
+        userService.updateWishlist(currentUser);
+
+
+        model.put("currentUser", currentUser);
+        return "redirect:/";
     }
 }
