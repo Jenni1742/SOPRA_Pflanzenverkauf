1e4cf9c0bebbb19750a96f618d1f204e67a49c1f
Jenni1742 <166017145+Jenni1742@users.noreply.github.com>
2024-06-08 19:53:08 +0200

    Seite und Controller für Käuferranking

---
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/BuyerRankingController.java
@@ -0,0 +1,31 @@
+package com.example.sopra_pflanzenverkauf.controller;
+
+import com.example.sopra_pflanzenverkauf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@Controller
+public class BuyerRankingController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Request Mapping for the Buyer Ranking Page.
+     *
+     * @param model the model.
+     * @return Buyer Ranking-page.
+     */
+    @RequestMapping(value = "/buyerRanking", method = RequestMethod.GET)
+    public String showBuyerRankingPage(Map<String, Object> model) {
+
+        model.put("currentUser", userService.getCurrentUser());
+
+        return "buyerRanking";
+    }
+}
+}
