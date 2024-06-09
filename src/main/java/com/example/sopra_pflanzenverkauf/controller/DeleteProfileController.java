632ed2548025d33d379d746aeec4540aed173040
st182291 <st182291@stud.uni-stuttgart.de>
2024-06-09 18:49:05 +0200

    Versuch den Fehler zu finden

--- src/main/java/com/example/sopra_pflanzenverkauf/controller/DeleteProfileController.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/DeleteProfileController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 //Hallo
+
 @Controller
 public class DeleteProfileController {
 
