1b5ddf8c58d16bf30f51f4887553186ae6b7e728
Jenni1742 <166017145+Jenni1742@users.noreply.github.com>
2024-06-14 21:04:55 +0200

    Alte Chat Klassen gelöscht

--- src/main/java/com/example/sopra_pflanzenverkauf/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sopra_pflanzenverkauf.repository;
-
-import com.example.sopra_pflanzenverkauf.entity.Message;
-import com.example.sopra_pflanzenverkauf.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
