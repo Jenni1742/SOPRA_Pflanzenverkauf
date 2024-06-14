3c787e35c19921894bc6bad7347187aee17d478b
Jenni1742 <166017145+Jenni1742@users.noreply.github.com>
2024-06-14 13:32:39 +0200

    Versuch Chat Problem zu lösen

--- src/main/java/com/example/sopra_pflanzenverkauf/controller/ChatJKController.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/ChatJKController.java
@@ -1,13 +1,20 @@
 package com.example.sopra_pflanzenverkauf.controller;
 
+import com.example.sopra_pflanzenverkauf.entity.ChatJK;
+import com.example.sopra_pflanzenverkauf.entity.Plant;
+import com.example.sopra_pflanzenverkauf.entity.User;
 import com.example.sopra_pflanzenverkauf.service.ChatJKService;
 import com.example.sopra_pflanzenverkauf.service.MessageJKService;
 import com.example.sopra_pflanzenverkauf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -30,10 +37,30 @@ public class ChatJKController {
      * @return overview-page.
      */
     @RequestMapping(value = "/chatJK", method = RequestMethod.GET)
-    public String showMyUserprofilePage(Map<String, Object> model) {
+    public String showChatJKPage(Model model) {
 
-        model.put("currentUser", userService.getCurrentUser());
+        User currentUser = userService.getCurrentUser();
+
+        List<ChatJK> chatList = currentUser.getReceivedChat();
+        chatList.addAll(currentUser.getSentChat());
+
+        System.out.println(chatList.size());
+
+        model.addAttribute("chatList", chatList);
+
+        model.addAttribute("currentUser", currentUser);
 
         return "chatJK";
     }
+
+    @GetMapping("/showSpecificChat")
+    public String showHome(Model model) {
+
+        User currentUser = userService.getCurrentUser();
+
+        model.addAttribute("currentUser", currentUser);
+
+        return "chatJK";
+    }
+
 }
