fd9f0d0f5e4b6ed10c4a76ddabca501f9b30712e
st183124 <st183124@stud.uni-stuttgart.de>
2024-06-30 16:18:10 +0200

    Es musste tatsächlich doch nochmal was geändert werden weil es mich sonst nicht mehr auf die Datenbank zugreifen lassen wollte. Das Problem mit der Merkliste bzw. Datenbank hängt irgendwie mit dem Csfr Token zusammen aber da blick ich noch nicht 100% durch. Stand jetzt funktioniert aber mit dieser Config alles.

--- src/main/java/com/example/sopra_pflanzenverkauf/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/configuration/SecurityConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -23,7 +22,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/login", "/register", "/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/console/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
@@ -47,7 +46,7 @@ public class SecurityConfiguration {
                         .deleteCookies("JSESSIONID")
                         .permitAll()
                 )
-                .headers(headers -> headers. frameOptions(Customizer.withDefaults()) // Ermöglicht das Einbetten der H2-Konsole in Frames
+                .headers(headers -> headers.frameOptions().sameOrigin() // Ermöglicht das Einbetten der H2-Konsole in Frames
                 );
 
         // Deaktiviert header security. Ermöglicht Nutzung der H2 Console.
