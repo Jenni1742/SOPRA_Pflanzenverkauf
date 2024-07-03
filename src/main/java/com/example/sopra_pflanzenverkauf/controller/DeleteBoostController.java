20142494a6a141d2f445ae5e6c4f0f421397ea3f
Jenni1742 <166017145+Jenni1742@users.noreply.github.com>
2024-07-03 23:39:59 +0200

    Boost delete über extraseite um zu bestätigen und kleinere Änderungen

---
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/DeleteBoostController.java
@@ -0,0 +1,72 @@
+package com.example.sopra_pflanzenverkauf.controller;
+
+import com.example.sopra_pflanzenverkauf.entity.Plant;
+import com.example.sopra_pflanzenverkauf.entity.User;
+import com.example.sopra_pflanzenverkauf.repository.PlantRepository;
+import com.example.sopra_pflanzenverkauf.service.PlantService;
+import com.example.sopra_pflanzenverkauf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class DeleteBoostController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PlantService plantService;
+
+    @Autowired
+    private PlantRepository plantRepository;
+
+
+    @GetMapping("/deleteBoost/{id}")
+    public String openDeletePlant(@PathVariable("id") Integer plantId, Model model) {
+
+        model.addAttribute("plantId", plantId);
+        model.addAttribute("currentUser", userService.getCurrentUser());
+
+        Plant plant = plantRepository.findById(plantId)
+                .orElse(null);
+
+        model.addAttribute("plant", plant);
+
+        if (plant == null) {
+            return "error/errorIDDoNotExist";
+        } else if (userService.getCurrentUser() == plant.getSeller()){
+            return "deleteBoost";
+        } else {
+            return "error/errorDeleteAdvertisement";
+        }
+
+    }
+
+
+    @PostMapping("/deleteBoost/{id}")
+    public String deletePlant(@PathVariable("id") Integer plantId, Model model) {
+
+        User currentUser = userService.getCurrentUser();
+
+        Plant plant = plantService.findById(plantId);
+
+        plant.setBooster(false);
+        plantService.save(plant);
+
+        List<Plant> plantList = currentUser.getPlantsToSell();
+
+        model.addAttribute("plantList", plantList);
+        model.addAttribute("currentUser", currentUser);
+        model.addAttribute("coinCount", currentUser.getPlantCoinCount());
+
+
+        return "redirect:/myAdvertisements";
+
+    }
+}
