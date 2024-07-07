23fd8e11ad14e6eeaa7a43e20d23605ab6b3b39a
Jenni1742 <166017145+Jenni1742@users.noreply.github.com>
2024-07-07 21:13:13 +0200

    Auskommentierter Code für Sortierung der Chat Übersicht

--- src/main/java/com/example/sopra_pflanzenverkauf/controller/ChatController.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/ChatController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -35,12 +39,93 @@ public class ChatController {
     @RequestMapping(value = "/chat", method = RequestMethod.GET)
     public String showChatPage(Model model) {
 
+        System.out.println("A");
         User currentUser = userService.getCurrentUser();
 
         List<Chat> chatList = currentUser.getReceivedChat();
         chatList.addAll(currentUser.getSentChat());
+        model.addAttribute("chatList", chatList); //Auskommentieren
 
-        model.addAttribute("chatList", chatList);
+        System.out.println("B");
+
+
+        /*
+        if (!chatList.isEmpty()) {
+            System.out.println("G");
+
+            //Speichert alle leeren Chats in einer extra Liste
+
+            List<Chat> emptyChats = new ArrayList<>();
+            for (Chat chatEmpty : chatList) {
+                if (chatEmpty.getMessagesInChat().size() == 0) {
+                    emptyChats.add(chatEmpty);
+                    chatList.remove(chatEmpty);
+                }
+            }
+
+            if (chatList.isEmpty()) {
+                model.addAttribute("chatList", emptyChats);
+            } else {
+                //Sortiert nach neuster Nachricht
+                List<Chat> sortedChatList = new ArrayList<>();
+                LocalDateTime timestampActualNewest = chatList.getFirst().getMessagesInChat().getLast().getTimestamp();
+                LocalDateTime timestampOfChat;
+                Chat actualNewestChat = chatList.getFirst();
+
+                System.out.println("C");
+
+                while (!chatList.isEmpty()) {
+                    System.out.println("C111");
+
+                    for (Chat chat : chatList) {
+                        System.out.println("C222");
+
+                        System.out.println("C777");
+
+                        timestampOfChat = chat.getMessagesInChat().getLast().getTimestamp();
+                        System.out.println("C333");
+                        if (timestampActualNewest.isBefore(timestampOfChat)|| timestampActualNewest.equals(timestampOfChat)) {
+                            System.out.println("C444");
+                            timestampActualNewest = timestampOfChat;
+                            actualNewestChat = chat;
+                        }
+
+                        /* Alt
+                        if (!chat.getMessagesInChat().isEmpty()){
+                            System.out.println("C777");
+
+                            timestamp2 = chat.getMessagesInChat().getLast().getTimestamp();
+                            System.out.println("C333");
+                            if (timestamp.isBefore(timestamp2)|| timestamp.equals(timestamp2)) {
+                                System.out.println("C444");
+                                timestamp = timestamp2;
+                                actualNewestChat = chat;
+                            }
+                        }else{
+                            System.out.println("C555");
+                            actualNewestChat = chat;
+                        }*/
+
+/*
+                    }
+
+
+                    System.out.println("C666");
+                    sortedChatList.add(actualNewestChat);
+                    chatList.remove(actualNewestChat);
+                }
+                // Fügt ans Ende der Sortierten Liste die leeren Chats an
+/*
+                System.out.println("D");
+                sortedChatList.addAll(emptyChats);
+                model.addAttribute("chatList", sortedChatList);
+            }
+
+        }else {
+            System.out.println("F");
+            model.addAttribute("chatList", chatList);
+        }
+*/
 
         model.addAttribute("currentUser", currentUser);
 
--- src/main/java/com/example/sopra_pflanzenverkauf/controller/ChatSpecificController.java
+++ src/main/java/com/example/sopra_pflanzenverkauf/controller/ChatSpecificController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -41,6 +42,7 @@ public class ChatSpecificController {
                               @RequestParam(value = "plantToBuyName", required = false) String plantToBuyName,
                               Model model) {
 
+        System.out.println("XXX");
         Plant plant = null;
 
         for (Plant plantOfList : plantService.getAllPlants()) {
@@ -50,6 +52,7 @@ public class ChatSpecificController {
                 }
             }
         }
+        System.out.println("YYY");
 
 
         User recipient = userService.getUserByUsername(recipientUsername);
@@ -72,9 +75,11 @@ public class ChatSpecificController {
             }
         }*/
 
+        System.out.println("ZZZ");
 
 
         if(chat1 == null) {
+            System.out.println("NNN");
         Chat chatObject = new Chat();
         chatObject.setRecipientOfChat(recipient);
         chatObject.setSenderOfChat(currentUser);
@@ -90,7 +95,10 @@ public class ChatSpecificController {
             Collections.reverse(messages);
             model.addAttribute("messages", messages);
 
+
+
         } else {
+            System.out.println("EEE");
             model.addAttribute("specificChat", chat1 );
             model.addAttribute("chatId", chat1.getChatId());
             model.addAttribute("chatPlant", chat1.getChatPlant());
@@ -102,6 +110,46 @@ public class ChatSpecificController {
         }
         model.addAttribute("currentUser", userService.getCurrentUser());
 
+        System.out.println("AAA");
+
+        List<Chat> chatList = currentUser.getReceivedChat();
+        chatList.addAll(currentUser.getSentChat());
+        model.addAttribute("chatList", chatList); //Auskommentieren
+
+        System.out.println("BBB");
+
+        /*
+        List<Chat> sortedChatList = new ArrayList<>();
+        LocalDateTime timestamp = chatList.getFirst().getMessagesInChat().getLast().getTimestamp();
+        System.out.println("CCC");
+        LocalDateTime timestamp2;
+        Chat newestChat = chatList.getFirst();
+        System.out.println("DDD");
+
+        while (!chatList.isEmpty()) {
+            System.out.println("h111");
+            for (Chat chat : chatList) {
+                System.out.println("h222");
+
+                if (!chat.getMessagesInChat().isEmpty()){
+                    System.out.println("h777");
+                    timestamp2 = chat.getMessagesInChat().getLast().getTimestamp();
+                    System.out.println("h333");
+                    if (timestamp.isBefore(timestamp2)|| timestamp.equals(timestamp2)) {
+                        System.out.println("h444");
+                        timestamp = timestamp2;
+                        newestChat = chat;
+                    }
+                }else{
+                    System.out.println("h555");
+                    newestChat = chat;
+                }
+            }
+            System.out.println("h666");
+            sortedChatList.add(newestChat);
+            chatList.remove(newestChat);
+        }
+        model.addAttribute("chatList", sortedChatList);*/
 
 
         return "redirect:/chat";
@@ -125,6 +173,7 @@ public class ChatSpecificController {
 
             model.addAttribute("chatPlant", chat.getChatPlant());
 
+            System.out.println("LOL");
             System.out.println(chat.getMessagesInChat().size());
 
             List<Message> messages = chat.getMessagesInChat();
@@ -133,6 +182,45 @@ public class ChatSpecificController {
 
 
             model.addAttribute("currentUser", currentUser);
+
+            List<Chat> chatList = currentUser.getReceivedChat();
+            chatList.addAll(currentUser.getSentChat());
+            model.addAttribute("chatList", chatList); //Auskommentieren
+
+            /*
+            List<Chat> sortedChatList = new ArrayList<>();
+
+            sortedChatList.add(chatList.get(chatList.indexOf(chat)));
+            chatList.remove(chat);
+
+
+            LocalDateTime timestamp = chatList.getFirst().getMessagesInChat().getLast().getTimestamp();
+            LocalDateTime timestamp2;
+            Chat newestChat = chatList.getFirst();
+            while (!chatList.isEmpty()) {
+                System.out.println("G111");
+                for (Chat chat3 : chatList) {
+                    System.out.println("g222");
+
+                    if (!chat3.getMessagesInChat().isEmpty()){
+                        System.out.println("g777");
+                        timestamp2 = chat3.getMessagesInChat().getLast().getTimestamp();
+                        System.out.println("g333");
+                        if (timestamp.isBefore(timestamp2)|| timestamp.equals(timestamp2)) {
+                            System.out.println("g444");
+                            timestamp = timestamp2;
+                            newestChat = chat3;
+                        }
+                    }else{
+                        System.out.println("g555");
+                        newestChat = chat3;
+                    }
+                }
+                System.out.println("g666");
+                sortedChatList.add(newestChat);
+                chatList.remove(newestChat);
+            }
+            model.addAttribute("chatList", sortedChatList);*/
 
             return "chatSpecific";
         } else {
@@ -145,6 +233,7 @@ public class ChatSpecificController {
                                @RequestParam("content") String content,
                                Model model){
 
+        User currentUser = userService.getCurrentUser();
         Chat chat = chatService.getChatByChatId(chatId);
 
         Message message = new Message();
@@ -171,6 +260,45 @@ public class ChatSpecificController {
         List<Message> messages = chat.getMessagesInChat();
         Collections.reverse(messages);
         model.addAttribute("messages", messages);
+
+        List<Chat> chatList = currentUser.getReceivedChat();
+        chatList.addAll(currentUser.getSentChat());
+        model.addAttribute("chatList", chatList); //Auskommentieren
+
+        /*
+        List<Chat> sortedChatList = new ArrayList<>();
+
+
+        sortedChatList.add(chatList.get(chatList.indexOf(chat)));
+        chatList.remove(chat);
+
+        LocalDateTime timestamp3 = chatList.getFirst().getMessagesInChat().getLast().getTimestamp();
+        LocalDateTime timestamp2;
+        Chat newestChat = chatList.getFirst();
+        while (!chatList.isEmpty()) {
+            System.out.println("P111");
+            for (Chat chat3 : chatList) {
+                System.out.println("P222");
+
+                if (!chat3.getMessagesInChat().isEmpty()){
+                    System.out.println("P777");
+                    timestamp2 = chat3.getMessagesInChat().getLast().getTimestamp();
+                    System.out.println("P333");
+                    if (timestamp3.isBefore(timestamp2)|| timestamp3.equals(timestamp2)) {
+                        System.out.println("P444");
+                        timestamp3 = timestamp2;
+                        newestChat = chat3;
+                    }
+                }else{
+                    System.out.println("P555");
+                    newestChat = chat3;
+                }
+            }
+            System.out.println("P666");
+            sortedChatList.add(newestChat);
+            chatList.remove(newestChat);
+        }
+        model.addAttribute("chatList", sortedChatList);*/
 
         return "redirect:/chatSpecific/{chatId}";
     }
